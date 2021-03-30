f3781a5044db18bb205eb038d3ef9223dbf3f21f
wietsebuseyne <wietsebuseyne@gmail.com>
2021-03-30 22:28:55 +0200

    Streamline hex foreground color (#32)

--- lib/presentation/core/color.dart
+++ lib/presentation/core/color.dart
@@ -1,14 +1,17 @@
 import 'dart:math';
 import 'dart:ui';
 
-bool useWhiteForeground(Color color, {double bias: 1.0}) {
-  // Old:
+import 'package:flutter/material.dart';
+
+bool useWhiteForeground(Color color) {
+  return ThemeData.estimateBrightnessForColor(color) == Brightness.dark;
+  // Oldest:
   // return 1.05 / (color.computeLuminance() + 0.05) > 4.5;
 
-  // New:
-  int v = sqrt(pow(color.red, 2) * 0.299 +
-      pow(color.green, 2) * 0.587 +
-      pow(color.blue, 2) * 0.114)
-      .round();
-  return v < 130 * bias ? true : false;
+  // Old:
+  // int v = sqrt(pow(color.red, 2) * 0.299 +
+  //     pow(color.green, 2) * 0.587 +
+  //     pow(color.blue, 2) * 0.114)
+  //     .round();
+  // return v < 130 * bias ? true : false;
 }
--- lib/presentation/players/player_presentation.dart
+++ lib/presentation/players/player_presentation.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:catan_master/domain/players/player.dart';
+import 'package:catan_master/presentation/core/color.dart';
 import 'package:flutter/material.dart';
 
 extension PlayerPresentation on Player {
@@ -9,6 +10,6 @@ extension PlayerPresentation on Player {
     return color == Colors.white ? Colors.black : color;
   }
 
-  Color get onColor => color == Colors.white ? Colors.black : Colors.white;
+  Color get onColor => useWhiteForeground(color) ? Colors.white : Colors.black;
 
 }
