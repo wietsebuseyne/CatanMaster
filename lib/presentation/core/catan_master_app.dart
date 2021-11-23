6f2d44ab549b289cedc8731386496ad69c64e4cc
wietsebuseyne <wietsebuseyne@gmail.com>
2021-11-23 21:18:28 +0100

    Theme improvements (dark + white colors)

--- lib/presentation/core/catan_master_app.dart
+++ lib/presentation/core/catan_master_app.dart
@@ -31,28 +31,46 @@ class CatanMasterApp extends StatelessWidget {
         return GamesPage(
           childBuilder: (context, gamesState) {
             //TODO keep list of possible colors and which are too light for primaryswatch
-            Color color = gamesState.games.getCatanMaster()?.color ?? Colors.blue;
-            MaterialColor? mc = colorToMaterialColor(color);
-            var light = isLight(color);
+            Color c = gamesState.games.getCatanMaster()?.color ?? Colors.blue;
+            MaterialColor mc = colorToMaterialColor(c) ?? Colors.blue;
+            var light = isLight(c);
             var overlayStyle = light ? SystemUiOverlayStyle.dark : SystemUiOverlayStyle.light;
             return MaterialApp(
               title: 'Catan Master',
               theme: ThemeData(
-                primaryColor: color,
+                brightness: Brightness.light,
+                primaryColor: c,
                 primarySwatch: mc,
                 appBarTheme: AppBarTheme(
-                  color: color,
+                  color: c,
                   systemOverlayStyle: overlayStyle,
-                ),
-                bottomNavigationBarTheme: BottomNavigationBarThemeData(
-                  selectedItemColor: light ? (mc?.shade900 ?? Colors.black) : color,
+                  foregroundColor: light ? Colors.black : Colors.white,
                 ),
                 dividerTheme: const DividerThemeData(indent: 32.0, endIndent: 32.0, thickness: 1.0),
+                bottomNavigationBarTheme: BottomNavigationBarThemeData(
+                  selectedItemColor: light ? mc.shade900 : mc,
+                ),
+                colorScheme: ColorScheme.fromSwatch(
+                  primarySwatch: mc,
+                  accentColor: c,
+                ),
               ),
               darkTheme: ThemeData(
-                brightness: Brightness.dark,
+                primaryColor: c,
                 primarySwatch: mc,
-                primaryColor: color,
+                appBarTheme: AppBarTheme(
+                  color: c,
+                  systemOverlayStyle: overlayStyle,
+                  foregroundColor: light ? Colors.black : Colors.white,
+                ),
+                dividerTheme: const DividerThemeData(indent: 32.0, endIndent: 32.0, thickness: 1.0),
+                bottomNavigationBarTheme: BottomNavigationBarThemeData(
+                  selectedItemColor: !light ? mc.shade200 : mc,
+                ),
+                colorScheme: ColorScheme.fromSwatch(
+                  primarySwatch: mc,
+                  accentColor: c,
+                ),
               ),
               home: CatanMasterHomeScreen(tab: state.page),
               initialRoute: '/',
